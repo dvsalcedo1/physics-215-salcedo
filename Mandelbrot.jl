ada44784c8b57403a8fc311eeb113626510c0595
Dylan Salcedo <salcedo.dylan@gmail.com>
2021-10-10 23:00:57 +0800

    Added mandelbrot file

---
+++ Mandelbrot.jl
@@ -0,0 +1,26 @@
+function mandelbrot(c)
+    z = c
+    itrMax = 80
+    for n in 1:itrMax
+        if abs(z) > 2
+            return n-1
+        end
+        z = z^2 + c
+    end
+    return itrMax
+end
+
+function plotMandelbrot( xrange::Tuple{Float64,Float64}, yrange::Tuple{Float64,Float64}; n=100 )
+    M = rand(n,n);
+    xval = range(xrange[1],xrange[2]; length=n);
+    yval = range(yrange[1],yrange[2]; length=n);
+    for i in 1:n, j in 1:n
+        M[i,j] = mandelbrot( xval[i] + im*yval[j] )
+    end
+    heatmap( M, c=:thermal
+        ,aspect_ratio=1.0
+        ,xaxis=nothing
+        ,yaxis=nothing
+    )
+end
+plotMandelbrot( vrange::Tuple{Float64,Float64}; n=100 ) = plotMandelbrot( vrange, vrange; n )
